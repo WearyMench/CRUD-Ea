784c711aeb96c8ebaad0cd30fa52875954961e96
Adrian Mirabal <adrian.mirabal07@gmail.com>
2024-05-30 19:56:45 -0400

    improbing database connection

--- src/data.js
+++ src/data.js
@@ -3,11 +3,24 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const connection = mysql.createConnection({
-  host: process.env.HOST,
-  database: process.env.DATABASE,
-  user: process.env.USER,
-  password: process.env.PASSWORD,
-});
+const { HOST, DBPORT, DATABASE, USER, PASSWORD } = process.env;
 
-export const getConnection = async () => await connection;
+if (!HOST || !DBPORT || !DATABASE || !USER || !PASSWORD) {
+  throw new Error("Missing required database environment variables");
+}
+export const getConnection = async () => {
+  try {
+    const connection = await mysql.createConnection({
+      host: HOST,
+      port: DBPORT,
+      database: DATABASE,
+      user: USER,
+      password: PASSWORD,
+    });
+    console.log("Connected to the database.");
+    return connection;
+  } catch (error) {
+    console.error("Error connecting to the database:", error);
+    throw error;
+  }
+};
